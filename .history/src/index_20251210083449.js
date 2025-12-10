d0c60cb96106936d01c5bf6f8aea71355c028a98
AG2904 <andresguilartelamuno@gmail.com>
2025-12-10 09:00:35 -0400

    Primera levantada del servidor

---
+++ .history/src/index_20251210083449.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+const port = 3005
+app.use(cors({ origin: '*' })) // cors
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ extended: false }))
+
+app.listen(port, function () {
+    console.log(`Api corriendo en http://localhost:${port}!`)
+})
